c2e1455ad8ddebcbe9063b66cfbae117c4c39db5
alaminizaan <96342811+alaminizaan@users.noreply.github.com>
2023-02-14 23:32:28 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
-
-app = Flask(__name__)
-
 @app.route('/')
 def home():
-    return 'Hello, world!'
+    opportunities = get_arbitrage_opportunities()
+    return f'''
+        <h1>Arbitrage Opportunities</h1>
+        <table>
+            <tr>
+                <th>Exchange</th>
+                <th>Coin Pair</th>
+                <th>Bid Price</th>
+                <th>Ask Price</th>
+                <th>Spread</th>
+                <th>Volume</th>
+            </tr>
+            {''.join([
+                f'<tr><td>{opportunity["exchange"]}</td><td>{opportunity["coin_pair"]}</td><td>{
+
+
+
+
+
+
+
