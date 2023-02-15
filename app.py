1fa3670f078c219377f1020e597932b88e3df0fd
alaminizaan <96342811+alaminizaan@users.noreply.github.com>
2023-02-15 10:23:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify
 binance_api_key = 'vpKO1DecEFexXzX5gsfG8WEmHuH5DPeV4lYqFKBJGwmjeVoWwXMZjU4UFcqQFaba'
 binance_secret = 'mnpTkA1KNYiDBpoUsiUjQGY3KUtPOxJHcad0aFVbvSF8h5imxhAaAcriP99oiMIi'
 
-exchanges = [{'name': 'binance', 'api_key': binance_api_key, 'secret': binance_secret}, 
+exchanges = [{'name': 'coinbase'}, 
              {'name': 'bitstamp'}]
 coins = ['BTC/USDT', 'ETH/USDT']
 
